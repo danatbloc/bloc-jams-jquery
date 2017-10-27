78ab2867dfff191f49557d5b66eb05a1f4fd0377
danielfsokol <danielfsokol@gmail.com>
2017-10-27 14:45:20 -0400

    add artist and release info

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -2,7 +2,7 @@ $(function(){
 
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
-  
-
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
 
 });
