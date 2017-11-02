37b05c35ff2ce39b7df7c3295b069fcec2e6fa2b
danielfsokol <danielfsokol@gmail.com>
2017-11-02 12:26:27 -0400

    Assignment-13-bloc-jams-completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -11,9 +11,20 @@ $(function(){
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  $('#previous').click(function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (prevSongIndex < 0) { return; }
+
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
   });
 
 
