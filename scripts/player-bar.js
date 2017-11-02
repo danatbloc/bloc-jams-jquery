f9396a3ea420e15c3573581dd43d42a5610c01d3
danielfsokol <danielfsokol@gmail.com>
2017-11-02 13:00:20 -0400

    Completed 14-bloc-jams

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -2,7 +2,7 @@ $(function(){
 
   $('#play-pause').click(function(){
     player.playPause();
-    $(this).attr('playState',player.playState)
+    $(this).attr('playState',player.playState);
   });
 
   $('#next').click(function(){
@@ -11,10 +11,22 @@ $(function(){
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function(event) {
+      player.skipTo(event.target.value);
+  });
+
+  setInterval( ()=>{
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100
+    console.log(percent);
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
 
 });
