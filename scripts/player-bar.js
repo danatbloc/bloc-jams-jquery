13883ab6168feb3388977e378b6a52b76825d77a
danielfsokol <danielfsokol@gmail.com>
2017-11-02 14:18:39 -0400

    assignment-14-bloc-jams completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,13 +13,28 @@ $(function(){
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    $('#time-control .total-time').text(nextSong.duration);
+    $('#time-control .total-time').text(player.prettyTime(parseInt(nextSong.duration)));
     player.playPause(nextSong);
+  });
+
+  $('#previous').click(function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (prevSongIndex < 0) { return; }
+
+    const prevSong = album.songs[prevSongIndex];
+    $('#time-control .total-time').text(player.prettyTime(parseInt(prevSong.duration)));
+    player.playPause(prevSong);
   });
 
   $('#play-pause').click(function(){
     const currentSongTotalTime = player.getDuration();
-    $('#time-control .total-time').text(currentSongTotalTime);
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const currentSong = album.songs[currentSongIndex];
+
+    $('#time-control .total-time').text(player.prettyTime(parseInt(currentSong.duration)));
   });
 
   $('#time-control input').on('input', function(event) {
@@ -30,7 +45,7 @@ $(function(){
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100
-    $('#time-control .current-time').text( currentTime );
+    $('#time-control .current-time').text( player.prettyTime(parseInt(currentTime)) );
     $('#time-control input').val(percent);
   }, 1000);
 
