cc91b8f7979c79fd3a257dae37d4209ecb29b0ce
danielfsokol <danielfsokol@gmail.com>
2017-11-02 12:17:43 -0400

    Completed 13-bloc-jams

---
+++ scripts/player-bar.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  $('#play-pause').click(function(){
+    player.playPause();
+    $(this).attr('playState',player.playState)
+  });
+
+  $('#next').click(function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
+    
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
+
+
+});
